5e79cc49e717feee558777dd6acd572960cfe383
vuxuanlai <vuxuanlai266@gmail.com>
2014-07-07 19:03:54 +0700

    the second comment

--- Beer.java
+++ Beer.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Beer{
 	public static void main(String asgs[]){
 		System.out.println("\t\t\t  The information of student\n");
-		System.out.println("VuXuanLai\t10020185\tK55CA\tlaivx_55\tlaivx_55@vnu.edu.vn\n");
+		System.out.println("VuXuanLai\t10020185\tK55CA\tlaivx\tlaivx_55@vnu.edu.vn\n");
 		for(int x=9;x>=1;x--){
 			System.out.printf("%d bottles of beer on the wall, %d bottles of beer\nTake one down, pass it around, \n",x,x);
 		}
